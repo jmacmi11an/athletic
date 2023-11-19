548e39adf8eddeb48e7bae60c5c015e1e43980e8
Jonathan C MacMillan <jmacmill@gmail.com>
2023-11-19 14:20:34 +1000

    Change component name from ArticleList to ArticleFeed

---
+++ react-app/src/containers/ArticleFeed.js
@@ -0,0 +1,67 @@
+import { gql } from 'apollo-boost';
+import { useQuery } from '@apollo/react-hooks';
+import React from 'react';
+import Card from './Card';
+import "../styles/ArticleFeed.css"
+
+const GET_ARTICLES = gql`
+  query {
+    articles {
+      id
+      title
+      author {
+        name
+      }
+      body
+      createdAt
+      team {
+        id
+        name
+      }
+      league {
+        id
+        name
+      }
+      imageUrlString
+    }
+  }
+`;
+
+export default function ArticleFeed({onClick, articleIdArray}) {
+  const { data, loading } = useQuery(GET_ARTICLES);
+
+  return (
+    <div className="ArticleFeed">
+      <div className='ArticleFeed-articles'>
+        {loading ? (
+          <p>Loading</p>
+        ) : (
+          <div className='Card-container'>
+              {articleIdArray && data.articles
+                .filter(article => (
+                  articleIdArray.length === 0
+                    ? true
+                    : articleIdArray.includes(article.team.id) || articleIdArray.includes(article.league.id) // this line of code nearly killed me
+                ))
+                .map(article => (
+                  <Card
+                    key={article.id}
+                    id={article.id} 
+                    img={article.imageUrlString}
+                    title={article.title}
+                    author={article.author.name}
+                    createdAt={article.createdAt}
+                    onClick={onClick}
+                  />
+                ))
+              }
+              <div></div>
+            </div>
+        )}
+        <div className='ArticleFeed-footer'>
+            Load More
+        </div>
+      </div>
+    </div>
+  );
+}
